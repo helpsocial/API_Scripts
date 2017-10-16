d922a185b37917544f5037e705d3f1a23fd4d090
Rob Collazo <robert@collazo.ws>
2017-10-16 07:51:44 -0500

    better organization and creation of new functions

--- auth.py
+++ auth.py
@@ -11,29 +11,37 @@ import json
 def baseurl():
     return 'https://api.helpsocial.com'
 
-def auth():
+def commonHeaders():
     parser = configparser.SafeConfigParser()
 
     parser.read('config.ini')
 
     scope = parser.get('account', 'scope')
     key = parser.get('account', 'key')
-    username = parser.get('account', 'username')
-    password = parser.get('account', 'password')
-
-    url = 'https://api.helpsocial.com'
-
-    body = {
-        'username': username,
-        'password': password
-    }
 
     headers = {
         'x-api-key': key,
         'x-auth-scope': scope,
         'content-type': "application/json"
     }
+    return headers
 
+def getUserToken():
+    url = baseurl()
+
+    parser = configparser.SafeConfigParser()
+
+    parser.read('config.ini')
+
+    username = parser.get('account', 'username')
+    password = parser.get('account', 'password')
+
+    body = {
+        'username': username,
+        'password': password
+    }
+
+    headers = commonHeaders()
     response = requests.post(url + "/2.0/tokens", headers=headers, json=body)
 
     if response.status_code == 401:
@@ -42,7 +50,25 @@ def auth():
         data = json.loads(response.content.decode('utf-8'))
 
         token = data['data']['token']['value']
-        headers['token'] = token
+        return token
 
-        return headers
-    
+def auth():
+    token = getUserToken()
+    headers = commonHeaders()
+    headers['x-auth-token'] = token
+
+    return headers
+
+def sseAuth():
+    url = baseurl()
+    headers = auth()
+
+    response = requests.get(url + "/2.0/streams/sse/authorization", headers=headers)
+
+    if response.status_code == 401:
+        print('[!] [{0}] Authentication Failed'.format(response.status_code))
+    else:
+        data = json.loads(response.content.decode('utf-8'))
+
+        token = data['data']['authorization']
+        return token
