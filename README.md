5e1e9551a5a62ad1c7dde56e885bd9b54d571ca4
Rob Collazo <robert@collazo.ws>
2017-10-16 07:53:24 -0500

    created new ssestream script

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Replace the appropriate values in config.ini.dist and rename or copy to config.ini
 
 stream.py - This will open a streaming connection to the HelpSocial API and will stream all activities in real time.
+ssestream.py - This will open a connection to the HelpSocial API SSE endpoint
---
+++ ssestream.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# Author: Robert Collazo <rob@helpsocial.com>
+# Copyright (c) 2017 HelpSocial, Inc.
+# See LICENSE for details
+
+import auth
+import json
+from sseclient import SSEClient
+
+url = auth.baseurl()
+sseToken = auth.sseAuth()
+
+messages = SSEClient(url + "/2.0/streams/sse?authorization=" + sseToken)
+
+for msg in messages:
+    print(msg)
